88114eeba0785877b5f27fc4afa25c5336f96560
shinshu68 <shinshu68@gmail.com>
2020-06-04 16:38:59 +0900

    :thumbsup: surveyorでタイルを合計いくらで買えるか計算するプログラムを作成開始

---
+++ tile_calc_by_surverors.py
@@ -0,0 +1,16 @@
+
+base_tile_cost = float(input('tile cost? (M) '))
+surveyors_cost = int(input('syrveyors cost? (M) '))
+russia_empire = input('russia empire? [y/n] ')
+
+surveyors_times = None
+for x in range(1, 11):
+    n = (x**2 - 2 * x + 2) * (2**((x - 1) // 10))
+    if surveyors_cost == n:
+        surveyors_times = x
+
+    if surveyors_cost < n:
+        print(f'Maybe Not Right Surveyors Cost Value ({surveyors_cost})')
+        exit()
+
+print(surveyors_times)
