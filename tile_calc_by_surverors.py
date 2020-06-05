9c4a060e5738c45dea83c0690dde04891d07af7f
shinshu68 <shinshu68@gmail.com>
2020-06-05 20:52:59 +0900

    :hammer: 値の入力周りを少し変更

--- tile_calc_by_surverors.py
+++ tile_calc_by_surverors.py
@@ -1,7 +1,10 @@
 
-base_tile_cost = float(input('tile cost? (M) '))
-surveyors_cost = int(input('surveyors cost? (M) '))
-russia_empire = input('russia empire? [y/n] ')
+base_tile_cost = float(input('Tile Cost? (M) '))
+surveyors_cost = int(input('Surveyors Cost? (M) '))
+russia_empire = input('Russia Empire? [y/n] ')
+while russia_empire not in ['y', 'n']:
+    print(f'Russia Empire Value is Invalid {russia_empire}')
+    russia_empire = input('Try Again: Russia Empire? [y/n] ')
 
 surveyors_times = None
 for x in range(1, 11):
