e274599cf340fe96365fe35b69272d1817c580f6
shinshu68 <shinshu68@gmail.com>
2020-06-06 14:15:29 +0900

    :black_nib: コメント追加

--- tile_calc_by_surverors.py
+++ tile_calc_by_surverors.py
@@ -1,4 +1,5 @@
 
+# 各種入力
 base_tile_cost = float(input('Tile Cost? (M) '))
 surveyors_cost = int(input('Surveyors Cost? (M) '))
 russia_empire = input('Russia Empire? [y/n] ')
@@ -6,6 +7,10 @@ while russia_empire not in ['y', 'n']:
     print(f'Russia Empire Value is Invalid {russia_empire}')
     russia_empire = input('Try Again: Russia Empire? [y/n] ')
 
+# タイルのコスト倍率を数値で保存
+tile_cost_ratio = 9 if russia_empire == 'y' else 10
+
+# surveyorsの回数を計算
 surveyors_times = None
 for x in range(1, 11):
     n = (x**2 - 2 * x + 2) * (2**((x - 1) // 10))
