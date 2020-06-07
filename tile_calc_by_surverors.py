7d5731a08bfe1eabb8e5b07337c4093966d8b826
shinshu68 <shinshu68@gmail.com>
2020-06-07 17:29:04 +0900

    :sparkles: surveyorの回数とtileの枚数を合計して出力するようにした

--- tile_calc_by_surverors.py
+++ tile_calc_by_surverors.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 # 各種入力
 base_tile_cost = float(input('Tile Cost? (M) '))
@@ -22,3 +23,36 @@ for x in range(1, 11):
         print(f'Maybe Not Right Surveyors Cost Value ({surveyors_cost})')
         exit()
 
+# 配列の大きさ指定
+r = 11
+c = 9 
+a = np.zeros((r, c))
+
+# surveyorが0回の時を計算
+for i in range(0, c):
+    a[0][i] = sum(base_tile_cost * tile_cost_ratio**x for x in range(0, i))
+
+# surveyorが1回以上の時を計算
+for i in range(1, r):
+    # n回surveyorした時のコスト合計
+    n = sum((x**2 - 2 * x + 2) * (2**((x - 1) // 10)) for x in range(surveyors_times, surveyors_times + i))
+    for j in range(0, c):
+        cost_cut = 2**i
+        a[i][j] = n + sum(base_tile_cost * (tile_cost_ratio**x) / cost_cut for x in range(0, j))
+
+
+axis_row = ['Tile →', 0, 1, 2, 3, 4, 5, 6, 7, 8]
+for i in axis_row:
+    print(f'{i:^9}', end='')
+
+print()
+print('Surveyor ↓')
+for i, li in enumerate(a):
+    print(f'{i:>9}', end='')
+    for x in li:
+        if x > 1e6:
+            break
+        print(f'{x:>8.1f} ', end='')
+    print()
+
+# print(a)
