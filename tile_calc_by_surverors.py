07de24d0d9d4890a5246cf81fc0244c785cbd07a
shinshu68 <shinshu68@gmail.com>
2020-06-07 17:34:16 +0900

    :sparkles: 入力をjson読み込みに変更

--- tile_calc_by_surverors.py
+++ tile_calc_by_surverors.py
@@ -1,15 +1,19 @@
 import numpy as np
+import json
+import os
 
-# 各種入力
-base_tile_cost = float(input('Tile Cost? (M) '))
-surveyors_cost = int(input('Surveyors Cost? (M) '))
-russia_empire = input('Russia Empire? [y/n] ')
-while russia_empire not in ['y', 'n']:
-    print(f'Russia Empire Value is Invalid {russia_empire}')
-    russia_empire = input('Try Again: Russia Empire? [y/n] ')
+HOME = os.getenv('HOME')
+
+with open(f'{HOME}/workspace/terrirory_idle/data.json') as f:
+    data = json.load(f)
+
+# 各種データ
+base_tile_cost = data['tile_cost']
+surveyors_cost = data['surveyors_cost']
+russia_empire = data['russia_empire']
 
 # タイルのコスト倍率を数値で保存
-tile_cost_ratio = 9 if russia_empire == 'y' else 10
+tile_cost_ratio = 9 if russia_empire else 10
 
 # surveyorsの回数を計算
 surveyors_times = None
